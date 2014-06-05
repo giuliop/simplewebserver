2201c4fe679a36b8d0a932c4cde060d078b69bb5
giulio <giuliop@gmail.com>
2014-06-05 15:22:33 +0200

    added option to redirect all traffic to https

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var port = flag.String("port", "8080", "you can specify which port to use for http traffic (default is 8080)")
 var ssl = flag.Bool("ssl", true, "you can specify if you want to listen for HTTPS traffic (default is true)")
 var ssl_port = flag.String("ssl_port", "8443", "you can specify which port to use for https traffic (default is 8443)")
+var secure = flag.Bool("secure", true, "you can specify if you want to redirect all http traffic to https (default is true)")
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -28,6 +29,11 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%v %v %v", r.RemoteAddr, r.Method, r.URL)
-	http.FileServer(http.Dir(".")).ServeHTTP(w, r)
+	// redirect all http traffic to https if 'secure' flag set
+	if *secure && r.TLS == nil {
+		http.Redirect(w, r, "https://"+r.Host+r.URL.Path, http.StatusFound)
+	} else {
+		log.Printf("%v %v %v", r.RemoteAddr, r.Method, r.URL)
+		http.FileServer(http.Dir(".")).ServeHTTP(w, r)
+	}
 }
