afe962f941313e3fce368551a94ab9ef1aa1e21b
giulio <giuliop@gmail.com>
2014-06-05 14:16:12 +0200

    now listening to both http and https

--- main.go
+++ main.go
@@ -6,12 +6,24 @@ import (
 	"net/http"
 )
 
-var port = flag.String("p", "8080", "you can specify which port to use (default is 8080)")
+var port = flag.String("port", "8080", "you can specify which port to use for http traffic (default is 8080)")
+var ssl = flag.Bool("ssl", true, "you can specify if you want to listen for HTTPS traffic (default is true)")
+var ssl_port = flag.String("ssl_port", "8443", "you can specify which port to use for https traffic (default is 8443)")
 
 func main() {
 	http.HandleFunc("/", handler)
 	flag.Parse()
-	log.Printf("listening on port %v...", *port)
+	startMsg := "listening on port " + *port + " (http)"
+	if *ssl {
+		startMsg += " and on port " + *ssl_port + " (https)"
+		go func() {
+			err := http.ListenAndServeTLS(":"+*ssl_port, "../ssl/giuliopizzini_com.crt", "../private/giuliopizzini.com.key.nopass", nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+	}
+	log.Printf("%v...", startMsg)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
