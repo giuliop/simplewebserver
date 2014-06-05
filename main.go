90b71522154136970e6162aae5c656172dde478d
giulio <giuliop@gmail.com>
2014-06-05 16:35:43 +0200

    made ssl key and certificate a command line parameter

--- main.go
+++ main.go
@@ -3,37 +3,47 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 )
 
-var port = flag.String("port", "8080", "you can specify which port to use for http traffic (default is 8080)")
-var ssl = flag.Bool("ssl", true, "you can specify if you want to listen for HTTPS traffic (default is true)")
-var ssl_port = flag.String("ssl_port", "8443", "you can specify which port to use for https traffic (default is 8443)")
-var secure = flag.Bool("secure", true, "you can specify if you want to redirect all http traffic to https (default is true)")
+var port = flag.String("port", "8080", "port to use for http traffic (default is 8080)")
+var ssl = flag.Bool("ssl", true, "whether you want to listen for HTTPS traffic (default is true)")
+var ssl_port = flag.String("ssl_port", "8443", "port to use for https traffic (default is 8443)")
+var ssl_cert = flag.String("ssl_cert", "ssl.crt", "file address of the ssl certificate (default is ssl.crt)")
+var ssl_key = flag.String("ssl_key", "ssl.key", "file address of the ssl private key (default is ssl.key)")
+var secure = flag.Bool("secure", true, "whether you want to redirect all http traffic to https (default is true)")
 
 func main() {
-	http.HandleFunc("/", handler)
 	flag.Parse()
+	http.HandleFunc("/", handler)
 	startMsg := "listening on port " + *port + " (http)"
 	if *ssl {
 		startMsg += " and on port " + *ssl_port + " (https)"
-		go func() {
-			err := http.ListenAndServeTLS(":"+*ssl_port, "../ssl/giuliopizzini_com.crt", "../private/giuliopizzini.com.key.nopass", nil)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
+		go listenSSL(*ssl_port, *ssl_cert, *ssl_key)
 	}
 	log.Printf("%v...", startMsg)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
+func listenSSL(port, cert, key string) {
+	err := http.ListenAndServeTLS(":"+port, cert, key, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	// redirect all http traffic to https if 'secure' flag set
-	if *secure && r.TLS == nil {
+	if *ssl && *secure && r.TLS == nil {
 		http.Redirect(w, r, "https://"+r.Host+r.URL.Path, http.StatusFound)
 	} else {
-		log.Printf("%v %v %v", r.RemoteAddr, r.Method, r.URL)
+		requester, err := net.LookupAddr(strings.Split(r.RemoteAddr, ":")[0])
+		if err != nil {
+			log.Print(err)
+		}
+		log.Printf("%v %v %v", requester, r.Method, r.URL)
 		http.FileServer(http.Dir(".")).ServeHTTP(w, r)
 	}
 }
